bfe914734f96303c7097c68421ae98c7d7baca1c
Christopher Johns <cjohns@scottlogic.com>
2024-04-04 14:19:19 +0100

    Removed python setup.cfg / py. Updated requirements.txt to match other collections.

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    py_modules=[],
-    name="digital-land-brownfield-site",
-    entry_points={"digital_land": ["brownfield_site_harmonise=pipeline.plugins:harmoniser_plugin"]},
-)
